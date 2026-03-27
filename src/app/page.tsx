8c40775b111cdc60da704e424f7fa60e0aea4642
shashank-kuldeep <shashankkuldeep04@gmail.com>
2026-03-27 18:42:23 +0530

    completed clerk authentication and configured proxy file for making the routes private

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client"
+import { SignInButton, SignOutButton, useUser } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
+  const { isSignedIn } = useUser();
+
   return (
     <div>
       HomePage
+
+      {!isSignedIn && <SignInButton />}
+      {isSignedIn && <SignOutButton />}
     </div>
   )
 }
