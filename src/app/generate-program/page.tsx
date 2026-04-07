ce2894916c61f5ceeec794c9f132bab162e85699
shashank-kuldeep <shashankkuldeep04@gmail.com>
2026-04-07 19:55:59 +0530

    vapi configuration remaining

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -1,22 +1,82 @@
-"use client";
+"use client"
+import { useState } from 'react'
+import {useUser} from '@clerk/nextjs'
+import { useRef } from "react";
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import { vapi } from '@/lib/vapi';
+import { useEffect } from 'react';
 
-import Vapi from "@vapi-ai/web";
+const GenerateProgramPage = () => {
+  const [callActive, setCallActive] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [messages,setMessages]=useState([]);
+  const [callEnded,setCallEnded]=useState(false);
 
-const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
+  const {user}=useUser();
+  const router=useRouter();
 
-export default function Page() {
-  const startCall = () => {
-    vapi.start(process.env.NEXT_PUBLIC_VAPI_ASSISTANT_ID!);
-  };
+  const messageContainerRef=useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    const handleCallStart=()=>{
+      console.log("call started");
+      setCallActive(true);
+      setConnecting(false);
+      setCallEnded(false);
+    }
+    const handleCallEnd=()=>{
+      console.log("call ended");
+      setCallActive(false);
+      setConnecting(false);
+      setIsSpeaking(false);
+      setCallEnded(true);
+    }
+    const handleSpeechStart=()=>{
+      console.log("AI started speaking");
+      setIsSpeaking(true);
+    }
+    const handleSpeechEnd=()=>{
+      console.log("AI stopped speaking");
+      setIsSpeaking(false);
+    }
+    const handleMessage=()=>{}
+    const handleError=(error:any)=>{
+      console.error("Error occurred:", error);
+      setCallActive(false);
+      setConnecting(false);
+    }
+
+
+
+
+    vapi.on("call-start",handleCallStart)
+        .on("call-end",handleCallEnd)
+        .on("speech-start",handleSpeechStart)
+        .on("speech-end",handleSpeechEnd)
+        .on("message",handleMessage)
+        .on("error",handleError)
+    
+    // clean up listeners
+
+    return ()=>{
+      vapi.off("call-start",handleCallStart)
+          .off("call-end",handleCallEnd)
+          .off("speech-start",handleSpeechStart)
+          .off("speech-end",handleSpeechEnd)
+          .off("message",handleMessage)
+          .off("error",handleError)
+    }
+  }, [])
+  
 
   return (
-    <div className="p-6">
-      <button
-        onClick={startCall}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Start AI Call
-      </button>
+    <div>
+      GenerateProgramPage
     </div>
-  );
-}
+  )
+};
+
+export default GenerateProgramPage;
